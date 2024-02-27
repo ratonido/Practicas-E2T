bab503e28ac5440b65c923bb6812cded2154944a
Ratonaco <atico83@gmail.com>
2024-02-27 10:18:41 +0100

    exer

---
+++ Python/Objetos/cesar.py
@@ -0,0 +1,25 @@
+"""Construir una clase para hacer cifrado cesar
+el cifrado cesar funciona por desplazamiento, es decir
+
+si nuestro cifrado tiene desplazamiento 3, desplaza 3 posiciones la letra
+
+una A se convertiria en D (A->D)
+
+para descifrar con desplazamiento 3, la D se convertiría en A
+
+cuando tenemos un texto, podemos acceder a las letras de cada una de sus posiciones
+
+variable = "HOLA"
+variable[0] = "H"
+variable[1] = "0"
+
+podemos crear nuestro conversor
+conversor = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmñopqrstuvwxyz0123456789-_.,:;*-+"
+
+Si uso conversor.find("B") me devuelve la posicion de esa letra
+Si uso conversor[4] me devuelve la letra en esa posición
+
+- Construir con Programación Orientada a Objetos, una clase que realice cifrado cesar
+- Para instanciar la clase, le diré el desplazamiento a usar
+- Tendré una función para descifrar, otra para descifrar y otra para cambiar el desplazamiento
+"""
---
+++ Python/Objetos/ejercicio_clase.py
@@ -0,0 +1,9 @@
+"""Construir una clase para hacer cifrado cesar
+el cifrado cesar fucniona por desplazmiento, es decir si nuestro cofrado
+tiene desplazmiento 3 desplaza 3 posiciones. La letra A se convertiría en D (A->D)
+para descifrar con desplazamiento 3 la D se convertiría en A
+cuando tenemos un texto podemos acceder a las letras de cada una de sus posiciones 
+
+podemos crear nuestro propioo conversor 
+conversor=ABCDEFGHIJKLMNOPQRSTUV
+"""
