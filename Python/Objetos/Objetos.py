7ce6c920e42e8cc300d76222e53a2e6f59af8b34
Raton <atico83@gmail.com>
2024-02-27 18:42:17 +0100

    act

--- Python/Objetos/Objetos.py
+++ Python/Objetos/Objetos.py
@@ -1,26 +1,25 @@
 #Objetos
 
 #Defino con la sentencia class y el nombre, generalmente la primera en mayúscula
-class Militar:    
-    pais="España"   #Atributos de clase: nuestro objeto puede tener valores comunes a toda la clase Atributos de instancia. Palabras mágicas self e init.Self hace referencia a
-                    # las variables del propio objeto
-
-    
-    def __init__(self,nombre,apellidos,edad):
+class Militar:            
+                # en esta parte puedo definir propiedades pero es mejor hacerlo en el init o iniciador
+    def __init__(self,nombre,apellidos,edad): # Constructor(def __init__).Palabras mágicas self e init.
+                                              # Self hace referencia a las variables del propio objeto
         self.nombre= nombre
         self.apellido=apellidos
         self.edad=edad
         print("se ha creado el militar", self.nombre,apellidos,edad,"en",self.pais)
         
-    def desfila(self):
+    def desfila(self):          # creamos un método. es igual que una función pero la diferencia es que el método pertenece a un objeto.
         print("Ahora",self.nombre,"está desfilando")
     
     def flexiones(self,cuantas):
         print("Ahora",self.nombre,"está haciendo",cuantas,"flexiones")
         
 
-    @classmethod        # Cuando modificamos cosas de la clase creada
-    def cambia_pais(cls,nuevo): 
+    @classmethod        # Cuando modificamos cosas de la clase creada, como hacer uso de una clase dentro de una misma clase y crear
+                        #otro objeto con las propiedades que le defina dentro del metodo 
+    def cambia_pais(cls,nuevo): # Con classmethod se usa cls en lugar de self
         cls.pais=nuevo
         
     def cumple(self):
@@ -29,16 +28,17 @@ class Militar:
         
     def nacimiento(self):
         print("El año de nacimiento de",self.nombre,'es',2024-self.edad)
-    @staticmethod           #Decorador, cuando no necesita atributos de la clase ni de la instancia
+    @staticmethod           # Decorador, cuando no necesita atributos de la clase ni de la instancia
     
     def frase(variable):
+        variable='hola'
         print('Dime algo' + variable)
     
 
-manuel=Militar('Manuel','Raton',30)
+manuel=Militar('Manuel','Raton',30) #se le da nombre al objeto junto con el nombre de la clase a la que pertenece
 jorge=Militar('Jorge','gomez',20)
 
-jorge.cumple()
+jorge.cumple() # Con esta llamada al método, cambiará el valor de la propiedad definida en la clase
 manuel.cumple()
 jorge.nacimiento()
 manuel.nacimiento()
