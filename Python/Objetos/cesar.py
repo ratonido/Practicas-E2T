e776dd165b71ef6457b0e0c675369e1dbced12fa
Ratonaco <atico83@gmail.com>
2024-02-27 13:16:39 +0100

    act

--- Python/Objetos/cesar.py
+++ Python/Objetos/cesar.py
@@ -23,3 +23,36 @@ Si uso conversor[4] me devuelve la letra en esa posición
 - Para instanciar la clase, le diré el desplazamiento a usar
 - Tendré una función para descifrar, otra para descifrar y otra para cambiar el desplazamiento
 """
+
+
+  
+
+class cifrado:    
+    
+    diccionario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmñopqrstuvwxyz0123456789-_.,:;*-+"
+    
+def __init__(self,desplazamiento):
+    self.desplazamiento = desplazamiento
+        
+    def cifrado(self,texto):
+        for i in texto:
+            
+            
+        
+    
+ 
+     
+        
+
+ 
+            
+        
+        
+        
+        
+        
+     
+        
+       
+        
+
