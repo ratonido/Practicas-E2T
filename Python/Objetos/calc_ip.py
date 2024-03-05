4ed99ff0bfa0643086f8bc982637d97bbcfd1934
Ratonaco <atico83@gmail.com>
2024-03-05 10:21:07 +0100

    act

--- Python/Objetos/calc_ip.py
+++ Python/Objetos/calc_ip.py
@@ -1,34 +1,50 @@
+"""
 class network():
     def __init__ (self,net,mask):
         self.net=net
         self.mask=mask
     
-    def masc_ip(self):
-        self.oct1=self.net[0:3]
-        self.oct2=self.net[5:7]
-        self.oct3=self.net[9:11]
-        self.oct4=self.net[13:15]
-        print()
+    def red_ip(self):
+        self.oct1=bin(int(net[0:3]))
+        self.oct2=bin(int(net[4:7]))
+        self.oct3=bin(int(net[8:11]))
+        self.oct4=bin(int(net[12:15]))
+        self.mask1=bin(int(mask[0:3]))
+        self.mask2=bin(int(mask[4:7]))
+        self.mask3=bin(int(mask[8:11]))
+        self.mask4=bin(int(mask[12:15]))         
         
             
-            
-            
-            
-        
-            
-                   
+               
             
     
 
     
   
-net=str(input("Introduce la dicrección IP"))
+net=str(input("Introduce la dirección IP con octetos completos"))
 mask=str(input("Introduce la máscara de red"))
 direccion=network(net,mask) 
-direccion.masc_ip()
+direccion.red_ip()
+"""
 
+   
+ 
+    
+    
+
+
+
+
+net=input("Introduce la dirección de red")
+red=net.split(".")
+mask=input("Introduce la máscara de red")
+mascara=mask.split(".")
+
+def ipsub(r,m):
     
     
     
-    
-    
+
+#mask=input("Introduce la máscara")
+
+#ipred(red)
