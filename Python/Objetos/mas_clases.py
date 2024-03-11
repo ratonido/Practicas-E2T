e2ac3111822beb0cc019c9c67e313fcecc901be0
Ratonaco <atico83@gmail.com>
2024-03-11 13:39:14 +0100

    act

---
+++ Python/Objetos/mas_clases.py
@@ -0,0 +1,63 @@
+# crear una clase estudiantes y otra clase cursos y manjear ambos
+
+
+
+
+alm=input("Introduce el nombre del alumno")
+ap=input("Introduce el apellido del alumno")
+dni=input("Introduce el dni del alumno")
+ed=input("Introduce la edad del alumno")
+curs=input("Introduce el nombre del surso")
+def alumno(nombre,apellidos,dni,edad):
+    for x in range(4):
+        inscripciones=[nombre,apellidos,dni,edad]
+    return nombre
+    
+def curso(nombre_curso):
+    Curso=nombre_curso
+    return Curso
+contador=1
+while True:
+    matriculado=alumno(alm,ap,dni,ed),"se ha matriculado en",curso(curs)
+    print(matriculado)
+    alm=input("Introduce el nombre del alumno")
+    ap=input("Introduce el apellido del alumno")
+    dni=input("Introduce el dni del alumno")
+    ed=input("Introduce la edad del alumno")
+    curs=input("Introduce el nombre del curso")
+    
+    contador-=1
+    if contador ==-1:
+        break
+    
+    
+    
+    
+    
+    
+    
+
+
+
+   
+        
+        
+
+        
+#ver cursos
+#ver estudiantes
+#agregar curso
+#agregar estudiante
+#inscribir alumno en curso
+#ver estudiantes cursos
+#finalizar
+       
+        
+        
+   
+            
+
+
+
+
+
